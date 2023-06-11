85ea3cadae6ed020bcd5a1910d894439b2831205
jxpose <apor.johnnichol00@icloud.com>
2023-06-11 15:10:21 +0900

    IDD

--- src/main/java/com/grandblue/webflux/models/response/GrandBlueResponse.java
+++ src/main/java/com/grandblue/webflux/models/response/GrandBlueResponse.java
@@ -2,8 +2,10 @@ package com.grandblue.webflux.models.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.util.List;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record GrandBlueResponse(ResponseData responseData, String[] errorMessages) {
+public record GrandBlueResponse(List<ResponseData> responseData, String[] errorMessages) {
 
   public static interface ResponseData {
   }
---
+++ src/main/java/com/grandblue/webflux/services/impl/ProductServiceImpl.java
@@ -0,0 +1,64 @@
+package com.grandblue.webflux.services.impl;
+
+import com.grandblue.webflux.models.db.ProductModel;
+import com.grandblue.webflux.models.requests.ProductRequest;
+import com.grandblue.webflux.repositories.ProductRepository;
+import com.grandblue.webflux.services.ProductService;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+  private final ProductRepository productRepository;
+
+  private static final boolean STATE_DELETED = true;
+
+  public ProductServiceImpl(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+
+  @Override
+  public Flux<ProductModel> getProduct() {
+    return productRepository.findAll();
+  }
+
+  @Override
+  public Mono<ProductModel> getProduct(String productId) {
+    return productRepository.findByProductId(UUID.fromString(productId));
+  }
+
+  @Override
+  public Mono<ProductModel> saveProduct(ProductRequest productRequest) {
+    ProductModel product = new ProductModel(
+        productRequest.productName(),
+        productRequest.productDescription()
+    );
+
+    return productRepository.save(product);
+  }
+
+  @Override
+  public Mono<ProductModel> updateProduct(String productId, ProductRequest productRequest) {
+    return productRepository.findByProductId(UUID.fromString(productId))
+        .flatMap(product -> {
+          product.setProductName(productRequest.productName());
+          product.setProductDescription(productRequest.productDescription());
+
+          return productRepository.save(product);
+        });
+  }
+
+  @Override
+  public Mono<ProductModel> deleteProduct(String productId) {
+    return productRepository.findByProductId(UUID.fromString(productId))
+        .flatMap(product -> {
+          product.setDeleteFlag(STATE_DELETED);
+          return productRepository.save(product);
+        });
+  }
+}
